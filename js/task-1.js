de45e6fc34ce285535fbe3a606440635b1fb44a7
Красюк Ігор <igorstein3@gmail.com>
2024-12-20 22:18:45 +0200

    first task

--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,20 @@
 'use strict';
+function isEnoughCapacity(products, containerSize) {
+  let totalAmountOfProducts = 0;
+  const amountsOfProducts = Object.values(products);
+  for (const amountOfProduct of amountsOfProducts) {
+    totalAmountOfProducts += amountOfProduct;
+  }
+  if (totalAmountOfProducts <= containerSize) {
+    return 'true';
+  } else {
+    return 'false';
+  }
+}
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
